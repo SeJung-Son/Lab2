e017dd3c4abd07e08f31a1ea2d1d40b113adf9b4
SeJung <patriate@snu.ac.kr>
2024-06-12 16:04:29 +0000

    My_Own_Code_SeJung_Son

--- main.cpp
+++ main.cpp
@@ -1,22 +1,34 @@
 #include<iostream>
-#include<vector>
+#include <vector>
 
 using namespace std;
 
-int N,M;
-vector<int> vec;
+void print_subset(vector<int> original_vec, vector<int> copied, int how_many) {
+    if (how_many == 0) {
+        for (auto elem : copied) {
+            cout << elem << " ";
+        }
+        cout << endl;
+        return;
+    }
 
-void func(int cnt) {
+    auto begin = original_vec.begin();
 
-
-
-
-
-
-
+    for (int i = 0; i <= original_vec.size() - how_many; i++) {
+        int mem = original_vec[i];
+        vector<int> new_vec2 = copied;
+        new_vec2.push_back(mem);
+        vector<int> new_vec(begin + i + 1, original_vec.end());
+        print_subset(new_vec, new_vec2, how_many - 1);
+    }
 }
-
+ 
 int main() {
-   cin >> N >> M;
-   func(0);
+    int N, M;
+    cin >> N >> M;
+    vector<int> initial, original; 
+    for (int i = 0; i < N; i++) {original.push_back(i+1);}
+    print_subset(original, initial, 5);
+    return 0;
 }
+
